86f8a90b1388c1cfe8d7f5eb9d44cb9d3c154dc0
Kleinst99 <71243898+Kleinst99@users.noreply.github.com>
2020-11-13 20:47:23 -0500

    Add files via upload
    Final code for controlling an LED with a hall effect sensor.

---
+++ arduino/hall_sensor_led.ino
@@ -0,0 +1,34 @@
+   //////////////////////////////////////////////
+  //        HALL EFFECT SENSOR DEMO           //
+ //          Author: Nick Koumaris           //
+//           http://www.educ8s.tv           //
+/////////////////////////////////////////////
+// using a1104lua-t //
+
+const int ledPin = 13;
+const int hallPin = 2;
+int hallState = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);
+  pinMode(hallPin,INPUT);
+  //attachInterrupt(digitalPinToInterrupt(hallPin), hall_ISR, CHANGE);
+}
+
+void loop() 
+{
+// read the state of the hall effect sensor:
+  hallState = digitalRead(hallPin);
+
+  if (hallState == LOW) {     
+    // turn LED on:    
+    digitalWrite(ledPin, HIGH);
+    Serial.println("Detect");
+  }   
+  else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW); 
+  }
+}
