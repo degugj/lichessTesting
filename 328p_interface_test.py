056dd9f236a4d0046d587d33c89aac4fb2d3e06f
jackd <jdeguglielmo@umass.edu>
2021-01-10 21:46:57 -0500

    Refactored names for numbered prefix files

--- 328p_interface_test.py
+++ 328p_interface_test.py
@@ -3,8 +3,8 @@
 #  WARNING: TEST WILL NOT WORK WITHOUT MODIFICATION TO GAMESTATE. CONSTRUCTOR MUST SUPPORT NONE FOR GAMEQUEUE
 import importlib
 from Engine.gameState import GameState as gs
-#from Engine.328p_interface import *
-interface = importlib.import_module('.328p_interface.328p_interface', 'Engine')
+#from Engine.x328p_interface import *
+interface = importlib.import_module('.x328p_interface.x328p_interface', 'Engine')
 
 currentGamestate = gs()  # Instantiate test gamestate
 move = 'd2d4'
