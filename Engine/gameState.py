7479bd3f703e06e6c5db33bd81f2f51b708a5364
weishanxli <weishanxli@gmail.com>
2021-01-10 20:47:03 -0500

    promotion working, alert window wip

--- Engine/gameState.py
+++ Engine/gameState.py
@@ -10,9 +10,10 @@ IMPORTS
 """
 import time
 
-import pygame as pg
+from Engine import chessboard
 
 from Engine.lichess import lichessInterface_new as interface
+from Engine.mcu_interfaces import fastScan_interface as fastScan
 
 
 """
@@ -25,30 +26,53 @@ class GameState():
 
         # set user color (i.e 'w', 'b')
         self.gameQueue = gameQueue
-        if self.gameQueue.get()["white"]["id"] == 'degugbot':     # <---------------------------------------------------------------------------------------DEGUGJ-----------------------------------
+        if self.gameQueue.get()["white"]["id"] == 'degugbot':
             self.userColor = 'w'
         else:
             self.userColor = 'b'
 
-        # letter and number conversion to index self.board
-        self.letter_to_y = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
-        self.number_to_x = {'1':7, '2':6, '3':5, '4':4, '5':3, '6':2, '7':1, '8':0}
+        # capture buffer zones
+        self.wBuffer = [["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"]]
 
-        # board state                      
-        self.board = [
-            ["bR", "bH", "bB", "bQ", "bK", "bB", "bH", "bR"],
-            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-            ["wR", "wH", "wB", "wQ", "wK", "wB", "wH", "wR"]]
+        self.bBuffer = [["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"],
+                        ["--", "--"]]
+        # dictionary that maps bishop to row 4, knight to row 5, rook to row 6, and queen to row 7
+        self.bufferMap = {'B':4, 'H':5, 'R':6, 'Q':7}
+        
+        # user is white
+        if self.userColor == 'w':
+            # letter and number conversion to index self.board
+            self.letter_to_y = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
+            self.number_to_x = {'1':7, '2':6, '3':5, '4':4, '5':3, '6':2, '7':1, '8':0}
 
-        self.userMove = True
+            # board state
+            self.board = [
+                ["bR", "bH", "bB", "bQ", "bK", "bB", "bH", "bR"],
+                ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+                ["--", "--", "--", "--", "--", "--", "--", "--"],
+                ["--", "--", "--", "--", "--", "--", "--", "--"],
+                ["--", "--", "--", "--", "--", "--", "--", "--"],
+                ["--", "--", "--", "--", "--", "--", "--", "--"],
+                ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+                ["wR", "wH", "wB", "wQ", "wK", "wB", "wH", "wR"]]
+
+            self.userMove = True
 
         # if user starts game as black
-        if self.userColor == 'b':
+        else:
             # letter and number conversion to index self.board
             self.letter_to_y = {'a':7, 'b':6, 'c':5, 'd':4, 'e':3, 'f':2, 'g':1, 'h':0}
             self.number_to_x = {'1':0, '2':1, '3':2, '4':3, '5':4, '6':5, '7':6, '8':7}
@@ -66,29 +90,7 @@ class GameState():
 
             self.userMove = False
 
-        self.defaultState = self.board
-
-
-        # capture buffer zones
-        self.whiteBuffer = [["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"]]
-
-        self.blackBuffer = [["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"],
-                            ["--", "--"]]
-        # dictionary that maps bishop to row 4, knight to row 5, rook to row 6, and queen to row 7
-        self.bufferMap = {'B':4, 'H':5, 'R':6, 'Q':7}
+        self.defaultState = self.board        
 
         # indicates if game is in first turn; used during get_opponentturn
         self.firstTurn = True
@@ -110,8 +112,15 @@ class GameState():
         return self.userMove
 
 
+# ---------------------------------------------------------------------------
+#   GAMESTATE MOVES AND CONDITIONS
+# ---------------------------------------------------------------------------
+
     """ make a move on local gamestate """
     def move_piece(self, move, castling = False):
+
+        # length of move string (normally 4, pawn promotion 5)
+        moveLength = len(move)
 
         # find starting cell in self.board
         startcell_y = self.letter_to_y[move[0]]
@@ -131,17 +140,21 @@ class GameState():
             if destpiece != "--":
                 self.capture_piece(destpiece)
             else:
-                # checks for en passant; returns corresponding captured pawn
-                capturePawn = enpassant(startpiece, destpiece, move)
-                if capturePawn != '':
-                    destpiece = capturePawn
-                    self.capture_piece(destpiece)
+                # check for pawn movement
+                if startpiece[1] == 'P':
+                    # check enpassant
+                    self.enpassant(destpiece, move)
 
-                # checks if castling; returns corresponding rook move if castling
+                # check if castling; returns corresponding rook move if castling
                 rookMove = self.castling(startpiece, move)
                 if rookMove != '':
                     self.move_piece(rookMove, castling=True)
 
+            # check for promotion
+            if moveLength == 5:
+                startpiece = self.promotion(startpiece, move)
+
+            
         self.board[startcell_x][startcell_y] = "--"
         self.board[destcell_x][destcell_y] = startpiece
 
@@ -155,14 +168,14 @@ class GameState():
             if piece[1] == 'P':
                 for row in range(4):
                     for column in range(2):
-                        if self.blackBuffer[row][column] == '--':
-                            self.blackBuffer[row][column] = piece
+                        if self.bBuffer[row][column] == '--':
+                            self.bBuffer[row][column] = piece
                             return
             # all pieces other than pawn; bishop, knight, rook, queen
             else:
                 for column in range(2):
-                    if self.blackBuffer[self.bufferMap[piece[1]]][column] == '--':
-                        self.blackBuffer[self.bufferMap[piece[1]]][column] = piece
+                    if self.bBuffer[self.bufferMap[piece[1]]][column] == '--':
+                        self.bBuffer[self.bufferMap[piece[1]]][column] = piece
                         return
 
         # if captured piece is white
@@ -171,14 +184,14 @@ class GameState():
             if piece[1] == 'P':
                 for row in range(4):
                     for column in range(2):
-                        if self.whiteBuffer[row][column] == '--':
-                            self.whiteBuffer[row][column] = piece
+                        if self.wBuffer[row][column] == '--':
+                            self.wBuffer[row][column] = piece
                             return
             # all pieces other than pawn; bishop, knight, rook, queen
             else:
                 for column in range(2):
-                    if self.whiteBuffer[self.bufferMap[piece[1]]][column] == '--':
-                        self.whiteBuffer[self.bufferMap[piece[1]]][column] = piece
+                    if self.wBuffer[self.bufferMap[piece[1]]][column] == '--':
+                        self.wBuffer[self.bufferMap[piece[1]]][column] = piece
                         return
         return
 
@@ -204,75 +217,152 @@ class GameState():
         return ''
 
 
+    """ handles pawn promotion """
+    def promotion(self, pawn, move):
+
+        # piece mapping
+        pieceDict = {'b':'B', 'k':'H', 'r':'R', 'q':'Q'}
+        # 5th element of string indicates promotion piece; (b, k, r, q) -> (B, H, R, Q)
+        promotionPiece = pieceDict[move[4]]
+
+        # check for user or opponent promotion
+        if self.userMove:
+
+            # put pawn in capture buffer and put return piece on board
+            self.capture_piece(pawn)
+            # find if piece is in return buffer
+            if pawn[0] == 'w':
+                for i in range(2):
+                    if self.wBuffer[self.bufferMap[pieceDict[move[4]]]][i] != '--':
+                        # remove promotion piece from white buffer and return it
+                        self.wBuffer[self.bufferMap[pieceDict[move[4]]]][i] = '--'
+                        return 'w' + promotionPiece
+
+            else:
+                for i in range(2):
+                    if self.bBuffer[self.bufferMap[pieceDict[move[4]]]][i] != '--':
+                        # remove promotion piece from black buffer and return it
+                        self.bBuffer[self.bufferMap[pieceDict[move[4]]]][i] = '--'
+                        return 'b' + promotionPiece
+
+        # opponent promotion
+        else:
+            
+            # put pawn in capture buffer
+            self.capture_piece(pawn)
+            # find if piece is in return buffer
+            if pawn[0] == 'w':
+                for i in range(2):
+                    if self.wBuffer[self.bufferMap[promotionPiece]][i] != '--':
+                        # remove promotion piece from white buffer and return it
+                        self.wBuffer[self.bufferMap[promotionPiece]][i] = '--'
+                        return 'w' + promotionPiece
+
+            else:
+                 for i in range(2):
+                    if self.bBuffer[self.bufferMap[promotionPiece]][i] != '--':
+                        # remove promotion piece from black buffer and return it
+                        self.bBuffer[self.bufferMap[promotionPiece]][i] = '--'
+                        return 'b' + promotionPiece
+            
+            print("Piece not in capture zone!")
+            self.promotion(pawn, move)
+
+
+
     """ handles en passant move by pawns """
-    def enpassant(self, piece, destpiece, move):
+    def enpassant(self, destpiece, move):
         # check if pawn has moved diagonally
         if move[0] != move[2]:
             # check for normal capture
-            if destpiece != '--':
+            if destpiece == '--': 
                 # en passant; find captured piece (i.e move=a2b3, capturedPiece=b2)
-                capturedPiece = self.board[letter_to_y[move[2]]][number_to_x[1]]
-                return capturedPiece
-        return ''
+                capturedPawn = self.board[self.number_to_x[move[1]]][self.letter_to_y[move[2]]]
+                self.capture_piece(capturedPawn)
 
+                # empty cell occupied by captured pawn
+                self.board[self.number_to_x[move[1]]][self.letter_to_y[move[2]]] = '--'
+
+        return
+
+
+# ---------------------------------------------------------------------------
+# GAME STATE UPDATES AND GETTING USER/OPPONENT MOVES
+# ---------------------------------------------------------------------------
 
     """ handles user/opponent moves and updates gamestate """
-    def update_gamestate(self):
+    def update_gamestate(self, screen):
 
         # user's move
         if self.userMove:
-            move = self.get_usermove()
-            self.move_piece(move)
-            self.userMove = False
+            chessboard.display_alert(screen, "User's Turn")
+            move = self.get_usermove(screen)
 
-            print("Opponent's Turn...")
+            # make move on local gamestate board
+            self.move_piece(move)
+
             return "ok"
 
         # opponent's move
         else:
-            move = self.get_opponentmove()
+            chessboard.display_alert(screen, "Opponent's Turn...")
+            move = self.get_opponentmove(screen)
 
-            # checks move
-            if move == "opponentresign":
-                return "opponentresign"
-            elif move == "none":
+            # move has not been received by opponent
+            if move == "none":
                 return "ok"
-            else:
-                print('Opponent move: ', move)
+            # move has been received
+            elif len(move) == 4 or len(move) == 5:
+                chessboard.display_alert(screen, "Opponent move: " + move)
+                # move piece on local gamestate board
                 self.move_piece(move)
                 self.userMove = True
                 return "ok"
+            else:
+                return move
 
 
     """ read local game state for user move """
-    def get_usermove(self):
+    def get_usermove(self, screen):
 
         """
         read local game state
         """
-        while 1:
-            print("User's turn - Enter Move:")
-            move = input()
+        move = input("User's turn - Enter Move: ")
 
-            valid = interface.make_move(move)
-            if valid:
-                return move
+        # check for pawn move and promotion
+        piece = self.board[self.number_to_x[move[1]]][self.letter_to_y[move[0]]]
+        if piece[1] == 'P' and (move[3] == '1' or move[3] == '8'):
+            while 1:
+                # prompt user for promotion piece
+                inputPiece = input("Promotion piece; bishop(b), knight(n), rook(r), queen(q): ")
+                if inputPiece == 'b' or inputPiece == 'k' or inputPiece == 'r' or inputPiece == 'q':
+                    break
+                chessboard.display_alert(screen, "Invalid promotion piece argument")
+            # append promotion piece to move
+            move += inputPiece
+
+        # send move to LiChess server
+        valid = interface.make_move(move, screen)
+        if valid:
+            self.userMove = False
+            return move
 
 
     """ read game stream for opponent move """
-    def get_opponentmove(self):
+    def get_opponentmove(self, screen):
 
         try:
             # get event from game queue and check the type of event
             event = self.gameQueue.get_nowait()
             if event["type"] == 'gameState':
-
+                print("OPPONENT EVENT", event)
                 # handles first turn
                 if self.firstTurn:
                     # user starts the game with first move; opponent is second
                     if self.userColor == 'w':
                         if len(event["moves"]) == 9:
-                            # get opponent's and save previous move
+                            # get opponent's move and save previous move
                             move = event["moves"].split()[-1]
                             self.firstTurn = False
                             self.previousMovesEvent = event
@@ -289,7 +379,7 @@ class GameState():
                 # handles all turns beyond the first
                 else:
                     # checks if the event is the opponent's; ignores user moves
-                    if len(event["moves"]) == len(self.previousMovesEvent["moves"]) + 10:                        
+                    if len(self.previousMovesEvent["moves"]) + 10 == len(event["moves"]) or len(self.previousMovesEvent["moves"]) + 11 == len(event["moves"]):                        
                         # get opponent's move and save previous move
                         move = event["moves"].split()[-1]
                         self.previousMovesEvent = event
@@ -298,75 +388,15 @@ class GameState():
                 # check for resignation
                 if event["status"] == "resign":
                     self.gameOver = True
-                    return "opponentresign"
+                    return event["winner"] + "resign"
+                if event["status"] == "mate":
+                    self.gameOver = True
+                    return event["winner"] + "mate"
             
         except:
             pass
 
         return "none"
-
-
-    """ handles when the game is over """
-    def end_game(self, event, resign=False, checkmate=False):
-
-
-
-        # init pygame window, window text, and icon
-        pg.init()
-        pg.display.set_caption("MagiChess: Game Over")
-        icon = pg.image.load("Engine/chessboard/chessboard_images/wQ.png")
-        pg.display.set_icon(icon)
-
-        screen = pg.display.set_mode((200, 200))
-        screen.fill(pg.Color("white"))
-        clock = pg.time.Clock()
-
-        font = pg.font.Font("freesansbold.ttf", size)
-        if resign:
-            textSurface = font.render("Game Over by resignation. Winner is " + event["winner"], True, color)
-
-        if checkmate:
-            textSurface = font.render("Game Over by checkmate. Winnder is " + event["winner"], True, color)
-
-        # align and display text
-        textBox = textSurface.get_rect()
-        textBox.center = 100, 100
-        screen.blit(textSurface, textBox)
-
-        # colors of the button
-        color_light = (170, 170, 170)
-        color_dark = (100, 100, 100)
-
-        run = True
-        while run:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    run = False
-                
-                if pg.type == pg.MOUSEBUTTONDOWN:
-                    if 100 <= mouse[0] <= 100 + 50 and 200/3 <= mouse[1] <= 200/3 + 20:
-                        pg.display.quit()
-                        pg.quit()
-
-
-            mouse = pg.mouse.get_pos()
-
-            if 100 <= mouse[0] <= 100 + 50 and 200/3 <= mouse[1] <= 200/3 + 20:
-                pygame.draw.rect(screen,color_light,[100,200/3,50,20])
-            else:
-                pygame.draw.rect(screen,color_dark,[100,200/3,50,20])
-
-            text = smallfont.render('Close', True, (255,255,255))
-            screen.blit(text, (100, 200/3))
-
-            clock.tick(15)
-            pg.display.flip()
-
-
-
-
-        return
-
 
 
     """ reset board to original state """ 
