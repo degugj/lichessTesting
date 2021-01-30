a97fc02de87e9644d9e09e6ce5df077ce27ed7ab
jackd <jdeguglielmo@umass.edu>
2021-01-30 15:34:46 -0500

    Removed illegal diagonal moves. Ready for physical testing

--- Engine/x328p_interface/x328p_interface.py
+++ Engine/x328p_interface/x328p_interface.py
@@ -43,7 +43,8 @@ class Node:
 
         if y - 1 >= 0 and x - 1 >= 0:
             child = map[self.pos[0]-1][self.pos[1]-1]
-            succs.append((child, 'sw'))
+            if map[self.pos[0] - 1][self.pos[1]].state == '. ' and map[self.pos[0]][self.pos[1] - 1].state == '. ':
+                succs.append((child, 'sw'))
 
         if y + 1 <= 26:
             child = map[self.pos[0]][self.pos[1]+1]
@@ -55,15 +56,19 @@ class Node:
 
         if x + 1 <= 16 and y + 1 <= 26:
             child = map[self.pos[0]+1][self.pos[1]+1]
-            succs.append((child, 'ne'))
+            if map[self.pos[0]+1][self.pos[1]].state == '. ' and map[self.pos[0]][self.pos[1]+1].state == '. ':
+                print(map[self.pos[0]+1][self.pos[1]].state)
+                succs.append((child, 'ne'))
 
         if x + 1 <= 16 and y - 1 >= 0:
             child = map[self.pos[0]+1][self.pos[1]-1]
-            succs.append((child, 'se'))
+            if map[self.pos[0]+1][self.pos[1]].state == '. ' and map[self.pos[0]][self.pos[1]-1].state == '. ':
+                succs.append((child, 'nw'))
 
         if x - 1 >= 0 and y + 1 <= 26:
             child = map[self.pos[0]-1][self.pos[1]+1]
-            succs.append((child, 'nw'))
+            if map[self.pos[0] - 1][self.pos[1]].state == '. ' and map[self.pos[0]][self.pos[1] + 1].state == '. ':
+                succs.append((child, 'se'))
 
         return succs
 
