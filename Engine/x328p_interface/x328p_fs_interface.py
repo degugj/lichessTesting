a9f4e52082e2578e4ac6aa1db78ec8f371dfdde4
degugj <jdeguglielmo@umass.edu>
2021-02-26 20:59:18 +0000

    Added some message type recognition to the code. Talk to Jack, code is functional but incomplete.

--- Engine/x328p_interface/x328p_fs_interface.py
+++ Engine/x328p_interface/x328p_fs_interface.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 from smbus import SMBus
 letterToColumn = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
-
+message_types = {'NA':0b00000111, 'Piece Picked Up':0b00001111, 'Piece Put Down':0b00010111, 'Invalid I2C Command':0b01011111}
 # I2C channel 1 is connected to the GPIO pins
 channel = 1
 #  MCP4725 defaults to address 0x60
@@ -26,18 +26,23 @@ def column_to_byte(column):
 
 def start_fast_scan(gs):
     newGs = np.array(gs.board)
-    
     return 0
 
 def stop_fast_scan():
     return 0
 
+def print_message_info(two_byte_message):
+    return 0
+
 def return_message_type(byteMessage):
     # Length check
     # Find type
-    print("Received data:", byteMessage, '(' + format(byteMessage, '#010b') + ')')
-
-    return 0
+    #print("Received data:", byteMessage, '(' + format(byteMessage, '#010b') + ')')
+    byte1 = byteMessage[0]
+    for key in message_types:
+        if (byte1&0b11111000) == (message_types[key]&0b11111000):
+            return key
+    return 'Unknown'
 
 def fast_scan_simulator():
     #https://raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2
@@ -45,13 +50,17 @@ def fast_scan_simulator():
     print("-- Fast Scanning Simulation Started --")
     while True:
             # Read 3 bytes of data via i2c
-            startMessage = 0x30
-            bus.write_byte(address, startMessage)
-            print("Transmitting Byte:",startMessage)
+            startMessageB1 = 0x30
+            startMessageB2 = 0xFF
+            #startMessage = [startMessageB1, startMessageB2]
+            bus.write_byte(address, startMessageB1)
+            #bus.write_byte(address, startMessageB2)
+            print("Transmitting Bytes:",startMessageB1)
             #time.sleep(.3)
-            recBytes = bus.read_byte(address)
+            recBytes = bus.read_i2c_block_data(address, 0, 2)
             #return_message_type(recBytes)
-            print("Recveived Byte:", recBytes)
+            print("Received Message Type:"+return_message_type(recBytes))
+            print("Recveived Bytes:", recBytes)
             time.sleep(5)
 
             # Write message via i2c
