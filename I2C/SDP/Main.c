6558a6053bb86e86fb97676454209ec3f65dc297
Kleinst99 <71243898+Kleinst99@users.noreply.github.com>
2021-03-11 00:36:04 -0500

    tried new i2c code

--- I2C/SDP/Main.c
+++ I2C/SDP/Main.c
@@ -4,20 +4,26 @@
  * Created: 2/22/2021 1:41:27 PM
  * Author : Samantha Klein
  * SDP 2021
- * code derived from embedds.com/programming-avr-i2c-interface/ and ATmega328 data sheet
+ * code derived from embedds.com/programming-avr-i2c-interface/ and ATmega328 data sheet and Peter Fleury's code
  */ 
 
 #ifndef TWI_H_
 #define TWI_H_
-#define ERROR 1
-#define SUCCESS (!ERROR)
+// #define ERROR 1
+// #define SUCCESS (!ERROR)
 #define EEDEVADR 0b10100000
+#define F_CPU 4000000UL
+#define SCL_CLOCK 100000L
+#define TWI_READ 1
+#define TWI_WRITE 0
 #include <util/twi.h>
 #include <avr/io.h>
+#include <inttypes.h>
 void TWIInit(void);
-void TWIStart(void);
+int TWIStart(void);
 void TWIStop(void);
-void TWIWrite(uint8_t u8data);
+int TWIWriteAddress(uint8_t u8addr);
+int TWIWrite(uint8_t u8data);
 uint8_t TWIReadACK(void);
 uint8_t TWIReadNACK(void);
 uint8_t TWIGetStatus(void);
@@ -25,29 +31,45 @@ uint8_t TWIGetStatus(void);
 #endif
 
 
-
 void TWIInit(void) // initialize TWI
 {
 	// set SCL to 400kHz
-	TWSR = 0x00;
-	TWBR = 0x0C;
+	TWSR = 0;
+	TWBR = ((F_CPU/SCL_CLOCK) - 16)/2;
 	// enable TWI
 	TWCR = (1<<TWEN);
+	
 }
 
-void TWIStart(void)
+int TWIStart(void)
 {
 	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
 	while (!(TWCR & (1<<TWINT)));
-	
+	uint8_t twst;
+	twst = TW_STATUS & 0xF8;
+	if((twst != TW_START) && (twst != TW_REP_START) ) return 1;
+	return 0;
 }
 
 void TWIStop(void)
 {
 	TWCR = (1<<TWINT) | (1<<TWSTO) | (1<<TWEN);
+	while(TWCR & (1<<TWSTO));
 }
 
-void TWIWrite(uint8_t u8data)
+int TWIWriteAddress(uint8_t u8addr) 
+{
+	TWDR = u8addr;
+	TWCR = (1<<TWINT) | (1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
+	uint8_t twst;
+	twst = TW_STATUS & 0xF8;
+	if((twst != TW_MT_SLA_ACK) && (twst != TW_MR_SLA_ACK)) return 1;
+	return 0;
+}
+
+
+int TWIWrite(uint8_t u8data)
 {
 	uint8_t twst;
 	TWDR = u8data;
@@ -81,25 +103,25 @@ uint8_t TWIGetStatus(void)
 	return status;
 }
 
-uint8_t EEWriteByte(uint16_t u16addr, uint8_t u8data)
+uint8_t EEWriteByte(uint8_t u8addr, uint8_t u8data)
 {
 	TWIStart();
-	//if(TWIGetStatus() != 0x08)
-	//return ERROR;
+	if(TWIGetStatus() != 0x08)
+	return 1;
 	// Select device and send A4 A1 A0 address bits
-	TWIWrite((EEDEVADR) | (uint8_t) ((u16addr)>>7));
-	//if(TWIGetStatus() != 0x18)
-	//return ERROR;
+	TWIWriteAddress(u8addr);
+	if(TWIGetStatus() != 0x18)
+	return 1;
 	// Send the rest of address
-	TWIWrite((uint8_t)(u16addr));
+	//TWIWrite((uint8_t)(u16addr));
 	//if(TWIGetStatus() != 0x28)
 	//return ERROR;
 	//write byte to eeprom
 	TWIWrite(u8data);
-// 	if(TWIGetStatus() != 0x28)
-// 	return ERROR;
+ 	if(TWIGetStatus() != 0x28)
+ 	return 1;
 	TWIStop();
-	return SUCCESS;
+	return 0;
 }
 
 int main(void)
@@ -107,7 +129,7 @@ int main(void)
 	TWIInit();
 	while(1)
 	{
-		TWIWrite(0x0F);
+		EEWriteByte(0x0F,0x0F);
 	}
 	
 }
