fbfae5680719cbd2189acfbbe2eb79350568d703
jackd <jdeguglielmo@umass.edu>
2021-04-13 15:15:49 -0400

    uncommenting serial import

--- Engine/x328p_interface/x328p_fs_interface.py
+++ Engine/x328p_interface/x328p_fs_interface.py
@@ -4,8 +4,8 @@
 import numpy as np
 from datetime import datetime
 import time
-#import serial
-#ser = serial.Serial("/dev/ttyS0", 9600)  # Open port with baud rate
+import serial
+ser = serial.Serial("/dev/ttyS0", 9600)  # Open port with baud rate
 letterToColumn = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5,'g': 6,'h': 7}  # To translate cell to posMap location
 columnToLetter = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
 message_types = {0b00000 : 'N/A', 0b00001: 'Piece Picked Up', 0b00010: 'Piece Put Down', 0b01011: 'Invalid I2C Command'}
